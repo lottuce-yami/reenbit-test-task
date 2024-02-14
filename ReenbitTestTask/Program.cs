8af9f6ea965b303db516047cc101d20f9b266f56
Lotte <lottuce.yami@gmail.com>
2024-02-14 13:37:12 +0100

    Setup configuration (env variables & user-secrets)

--- ReenbitTestTask/Program.cs
+++ ReenbitTestTask/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Configuration.AddEnvironmentVariables(prefix: "reenbit_");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
