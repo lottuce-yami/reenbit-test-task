8ada187609e99a63ff33b87ef05a319bf9ababa6
Lotte <lottuce.yami@gmail.com>
2024-02-15 22:11:49 +0100

    Add test for email sending method

--- ReenbitTestTaskAzureFunction/Services/EmailService.cs
+++ ReenbitTestTaskAzureFunction/Services/EmailService.cs
@@ -1,32 +1,27 @@
 ﻿using Azure.Communication.Email;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace ReenbitTestTaskAzureFunction.Services;
 
 public class EmailService
 {
-    private readonly ILogger<EmailService> _logger;
     private readonly IConfiguration _configuration;
-    
-    public EmailService(ILogger<EmailService> logger, IConfiguration configuration)
+    private readonly EmailClient _emailClient;
+
+    public EmailService(IConfiguration configuration, EmailClient? emailClient = null)
     {
-        _logger = logger;
         _configuration = configuration;
+        _emailClient = emailClient ?? new EmailClient(_configuration["COMMUNICATION_SERVICES_CONNECTION_STRING"]);
     }
 
-    public Task SendMail(string recipient, string subject, string message)
+    public void SendMail(string recipient, string subject, string message)
     {
-        var emailClient = new EmailClient(_configuration["COMMUNICATION_SERVICES_CONNECTION_STRING"]);
-
-        var emailSendOperation = emailClient.SendAsync(
+        _emailClient.SendAsync(
             Azure.WaitUntil.Completed,
             senderAddress: _configuration["SENDER_ADDRESS"],
             recipientAddress: recipient,
             subject: subject,
             htmlContent: message
         );
-
-        return Task.CompletedTask;
     }
 }
---
+++ ReenbitTestTaskTests/AzureFunction/EmailService/SendMail.cs
@@ -0,0 +1,55 @@
+﻿using Azure.Communication.Email;
+using Microsoft.Extensions.Configuration;
+using Moq;
+
+namespace ReenbitTestTaskTests.AzureFunction.EmailService;
+
+public class SendMail
+{
+    
+    [Fact]
+    public void SendsMailOnce()
+    {
+        // Arrange
+        var emailConfiguration = new Dictionary<string, string>
+        {
+            {"SENDER_ADDRESS", "DoNotReply@example.com"}
+        };
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(emailConfiguration!)
+            .Build();
+
+        var recipient = "test@example.com";
+        var subject = "Test Subject";
+        var message = "Test message";
+        
+        var mock = new Mock<EmailClient>();
+        mock.Setup(x => x.SendAsync(
+                It.IsAny<Azure.WaitUntil>(),
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<CancellationToken>()
+            )
+        );
+            
+        var service = new ReenbitTestTaskAzureFunction.Services.EmailService(configuration, mock.Object);
+        
+        // Act
+        service.SendMail(recipient, subject, message);
+        
+        // Assert
+        mock.Verify(x => x.SendAsync(
+                It.IsAny<Azure.WaitUntil>(),
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<CancellationToken>()
+            ), Times.Once
+        );
+    }
+}
