1aa07a2ca66fe8909551e144de98ce23961820b7
Lotte <lottuce.yami@gmail.com>
2024-02-15 23:41:16 +0100

    Add test for metadata retrieving

---
+++ ReenbitTestTaskTests/AzureFunction/BlobStorageTrigger/GetMetadata.cs
@@ -0,0 +1,30 @@
+﻿using Azure;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace ReenbitTestTaskTests.AzureFunction.BlobStorageTrigger;
+
+public class GetMetadata
+{
+    [Fact]
+    public void GetsMetadataCorrectly()
+    {
+        // Arrange
+        var metadata = new Dictionary<string, string>
+        {
+            {"recipient", "test@example.com"}
+        };
+        var blobProperties = BlobsModelFactory.BlobProperties(metadata: metadata);
+        
+        var mock = new Mock<BlobClient>();
+        var responseMock = new Mock<Response>();
+        mock.Setup(x => x.GetPropertiesAsync(null, CancellationToken.None).Result)
+            .Returns(Response.FromValue(blobProperties, responseMock.Object));
+        
+        // Act
+        var result = ReenbitTestTaskAzureFunction.BlobStorageTrigger.GetMetadata(mock.Object, "recipient");
+
+        // Assert
+        result.Should().Be(metadata["recipient"]);
+    }
+}
